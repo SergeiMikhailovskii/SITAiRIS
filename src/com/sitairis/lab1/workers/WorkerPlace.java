c4b96d2a3d93dd3dec1e3b502275c29c9f754c6f
Sergei Mikhailovskii <mikhailovskii.s@tut.by>
2019-09-29 23:23:38 +0300

    Added class WorkerPlace

---
+++ src/com/sitairis/lab1/workers/WorkerPlace.java
@@ -0,0 +1,45 @@
+package com.sitairis.lab1.workers;
+
+public class WorkerPlace {
+
+    private int placeId;
+    private int roomNumber;
+    private boolean isNearWindow;
+
+    public WorkerPlace() {
+        placeId = 0;
+        roomNumber = 0;
+        isNearWindow = false;
+    }
+
+    public WorkerPlace(int placeId, int roomNumber, boolean isNearWindow) {
+        this.placeId = placeId;
+        this.roomNumber = roomNumber;
+        this.isNearWindow = isNearWindow;
+    }
+
+    public int getPlaceId() {
+        return placeId;
+    }
+
+    public void setPlaceId(int placeId) {
+        this.placeId = placeId;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public boolean isNearWindow() {
+        return isNearWindow;
+    }
+
+    public void setNearWindow(boolean nearWindow) {
+        isNearWindow = nearWindow;
+    }
+
+}
