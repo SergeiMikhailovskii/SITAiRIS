1b300fcde70ec81ca1e292e533d9a7a26686b25d
Sergei Mikhailovskii <mikhailovskii.s@tut.by>
2019-09-29 23:00:19 +0300

    Added class AppliancesShop

---
+++ src/com/sitairis/lab1/AppliancesShop.java
@@ -0,0 +1,25 @@
+package com.sitairis.lab1;
+
+public class AppliancesShop extends Shop {
+
+    private int amountOfTestDevices;
+
+    public AppliancesShop() {
+        super();
+        amountOfTestDevices = 0;
+    }
+
+    public AppliancesShop(String address, String phoneNumber, String name, int amountOfTestDevices) {
+        super(address, phoneNumber, name);
+        this.amountOfTestDevices = amountOfTestDevices;
+    }
+
+    public int getAmountOfTestDevices() {
+        return amountOfTestDevices;
+    }
+
+    public void setAmountOfTestDevices(int amountOfTestDevices) {
+        this.amountOfTestDevices = amountOfTestDevices;
+    }
+
+}
