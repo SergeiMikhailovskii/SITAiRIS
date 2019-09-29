6a207008148496f93c1919b378fb9b6051cb1af6
Sergei Mikhailovskii <mikhailovskii.s@tut.by>
2019-09-29 22:57:05 +0300

    Added class Shop

---
+++ src/com/sitairis/lab1/Shop.java
@@ -0,0 +1,49 @@
+package com.sitairis.lab1;
+
+public class Shop {
+
+    private String address;
+    private String phoneNumber;
+    private String name;
+
+    public Shop() {
+        address = "Nezavisimosti avenue";
+        phoneNumber = "123-45-67";
+        name = "Hit";
+    }
+
+    public Shop(String address, String phoneNumber, String name) {
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void createNewPosition(){
+        System.out.println("New position created!");
+    }
+
+}
