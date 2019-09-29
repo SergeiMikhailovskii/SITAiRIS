b55624122cbe4fb7b3b19e4811527ee435b4a867
Sergei Mikhailovskii <mikhailovskii.s@tut.by>
2019-09-29 23:27:45 +0300

    Added class Category

---
+++ src/com/sitairis/lab1/item/Category.java
@@ -0,0 +1,44 @@
+package com.sitairis.lab1.item;
+
+public class Category {
+
+    private int categoryId;
+    private String name;
+    private String description;
+
+    public Category() {
+        categoryId = 0;
+        name = "Category 1";
+        description = "Description 1";
+    }
+
+    public Category(int categoryId, String name, String description) {
+        this.categoryId = categoryId;
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
