2ef7a09bd8ee05c3851c03127b3e1fe92426f7fd
Sergei Mikhailovskii <mikhailovskii.s@tut.by>
2019-09-30 22:42:43 +0300

    Added order creation

--- src/com/sitairis/lab1/Main.java
+++ src/com/sitairis/lab1/Main.java
@@ -1,10 +1,20 @@
 package com.sitairis.lab1;
 
+import com.sitairis.lab1.customers.Customer;
+import com.sitairis.lab1.item.Item;
+import com.sitairis.lab1.order.Order;
 import com.sitairis.lab1.shop.AppliancesShop;
+import com.sitairis.lab1.workers.Worker;
 
 public class Main {
 
     public static void main(String[] args) {
         AppliancesShop appliancesShop = new AppliancesShop();
+
+        Worker worker = new Worker();
+        Customer customer = new Customer();
+        Item item = new Item();
+
+        Order order = new Order(worker, customer, item);
     }
 }
